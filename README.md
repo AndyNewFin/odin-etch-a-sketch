0469feacc19d6c4d017ccc3bc5ad250cc6122018
AndyNewFin <andy_cnguyen@outlook.com>
2022-04-21 16:36:55 -0700

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # odin-etch-a-sketch
+4/16/2022 Started this project by writing out the instructions with some pseudocode for each step
+
+Worked on the project for 2-3 hours a day, trying to complete each of the steps
+-One issue I am learning about JS code is sometimes the entire JS has to be complete and correct in contrast to completing one step then trying another step
+-The Odin Project states "plenty of resources on the net for learning stuff that we haven’t covered yet" and to use Google for help... 
+	-but since the coding language is so vast and I'm still learning, I do not know exactly what to Google
+		- I did get a lot of help from Discord and Stack Overflow
+		
+Postitive: I am happy thar I was able to write out alot of the code before seeking help
+- I estimate I completed these percents before reaching outside help
+	-HTML 100%
+	-CC    80%
+	-JS    50%
+
+4/21/2022
+I am about 98% complete with this project! One of the issue that remains is when I click "New Grid"... some odd numbers like 7 or 9, the new grid will not align correctly
+	-I am still having perfecting my flexbox code! I created more containers then I needed too 1) Main container   2) Grid container      3) Button container
--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 // TOP Project: Etch-A-Sketch
+
+const container = document.querySelector(".container");
+const btnClear = document.querySelector("#clearBtn");
+const btnNew = document.querySelector("#newBtn");
+
+// fuction grid creates a forloop: starts with zero boxes highlighted and listens to an event of the mousse curser passing over; starts with a grid of 16 boxes
+function grid() {
+    for (let i = 0; i < 16; i++) {
+        for (let g = 0; g < 16; g++) {
+            let element = document.createElement("div");
+            element.className = "child";
+            element.addEventListener("mouseover", e => {
+                e.target.style.backgroundColor = "pink";
+            })
+            container.appendChild(element);
+        }
+    }
+}
+
+grid();
+
+function makeGrid(grid) {
+    container.innerHTML = "";
+    for (let i = 0; i < grid; i++) {
+        for (let g = 0; g < grid; g++) {
+            let element = document.createElement("div");
+            element.className = "child";
+            let numGridsize = 40 / grid;
+            element.style.width = `${numGridsize}rem`;
+            element.style.height = `${numGridsize}rem`;
+            element.addEventListener("mouseover", e => {
+                e.target.style.backgroundColor = "pink";
+            })
+            container.appendChild(element);
+        }
+    }
+}
+
+// Button that clears previous highlighting
+btnClear.addEventListener("click", () => {
+    let childs = document.querySelectorAll(".child");
+    childs.forEach(child => {
+        child.style.backgroundColor = "white";
+    });
+});
+
+// New Grid button that prompts Enter a New Number; Creates a new grid with user entered amount
+btnNew.addEventListener("click", () => {
+    let result = prompt("Enter a number", 16);
+    result = parseInt(result);
+    makeGrid(result);
+});
